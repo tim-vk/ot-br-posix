2ad031bf0ff1a577010bb734871cf6879072dce1
jwhui <jwhui@users.noreply.github.com>
2022-05-09 20:54:55 +0000

    deploy: ea1bb93b909ce43356adfc2f4814f58fa3cfacc7

--- search/all_3.js
+++ search/all_3.js
@@ -51,7 +51,7 @@ var searchData=
   ['decodetxtdata_106',['DecodeTxtData',['../da/ddc/classotbr_1_1Mdns_1_1Publisher.html#aeb3784d13ebd0b8e9d54f4f746d260ad',1,'otbr::Mdns::Publisher']]],
   ['deinit_107',['Deinit',['../d5/d01/structotbr_1_1Application.html#adf5dda38fabceb5ad6f81fbdca30a6f8',1,'otbr::Application::Deinit()'],['../d4/dfd/classotbr_1_1BorderAgent.html#a87d7411d961528f439d3e4115e40efc2',1,'otbr::BorderAgent::Deinit()'],['../db/d17/classotbr_1_1Ncp_1_1ControllerOpenThread.html#a8cdaf1c76ce40dee08edd2ce1a6363eb',1,'otbr::Ncp::ControllerOpenThread::Deinit()']]],
   ['destroy_108',['Destroy',['../da/ddc/classotbr_1_1Mdns_1_1Publisher.html#a764c8e33ecdf355a4eaf60292e9484c2',1,'otbr::Mdns::Publisher']]],
-  ['detach_109',['Detach',['../d4/d44/classotbr_1_1agent_1_1ThreadHelper.html#a7b818790e83d9ad3a330a80f96bc938f',1,'otbr::agent::ThreadHelper']]],
+  ['detach_109',['Detach',['../df/dcc/classotbr_1_1DBus_1_1ThreadApiDBus.html#a0a08eab9fc3fc1547316c41cb9e2be48',1,'otbr::DBus::ThreadApiDBus::Detach()'],['../d4/d44/classotbr_1_1agent_1_1ThreadHelper.html#a7b818790e83d9ad3a330a80f96bc938f',1,'otbr::agent::ThreadHelper::Detach()']]],
   ['diag2jsonstring_110',['Diag2JsonString',['../d0/d6c/namespaceotbr_1_1rest_1_1Json.html#a1d5fb81fc1a65c3d90406d6e1e9c5f35',1,'otbr::rest::Json']]],
   ['diaginfo_111',['DiagInfo',['../da/dfd/structotbr_1_1rest_1_1DiagInfo.html',1,'otbr::rest']]],
   ['discoveredhostcallback_112',['DiscoveredHostCallback',['../da/ddc/classotbr_1_1Mdns_1_1Publisher.html#aea1cfd72140d8d469b149ee279b44766',1,'otbr::Mdns::Publisher']]],
--- search/functions_3.js
+++ search/functions_3.js
@@ -10,7 +10,7 @@ var searchData=
   ['decodetxtdata_887',['DecodeTxtData',['../da/ddc/classotbr_1_1Mdns_1_1Publisher.html#aeb3784d13ebd0b8e9d54f4f746d260ad',1,'otbr::Mdns::Publisher']]],
   ['deinit_888',['Deinit',['../d5/d01/structotbr_1_1Application.html#adf5dda38fabceb5ad6f81fbdca30a6f8',1,'otbr::Application::Deinit()'],['../d4/dfd/classotbr_1_1BorderAgent.html#a87d7411d961528f439d3e4115e40efc2',1,'otbr::BorderAgent::Deinit()'],['../db/d17/classotbr_1_1Ncp_1_1ControllerOpenThread.html#a8cdaf1c76ce40dee08edd2ce1a6363eb',1,'otbr::Ncp::ControllerOpenThread::Deinit()']]],
   ['destroy_889',['Destroy',['../da/ddc/classotbr_1_1Mdns_1_1Publisher.html#a764c8e33ecdf355a4eaf60292e9484c2',1,'otbr::Mdns::Publisher']]],
-  ['detach_890',['Detach',['../d4/d44/classotbr_1_1agent_1_1ThreadHelper.html#a7b818790e83d9ad3a330a80f96bc938f',1,'otbr::agent::ThreadHelper']]],
+  ['detach_890',['Detach',['../df/dcc/classotbr_1_1DBus_1_1ThreadApiDBus.html#a0a08eab9fc3fc1547316c41cb9e2be48',1,'otbr::DBus::ThreadApiDBus::Detach()'],['../d4/d44/classotbr_1_1agent_1_1ThreadHelper.html#a7b818790e83d9ad3a330a80f96bc938f',1,'otbr::agent::ThreadHelper::Detach()']]],
   ['diag2jsonstring_891',['Diag2JsonString',['../d0/d6c/namespaceotbr_1_1rest_1_1Json.html#a1d5fb81fc1a65c3d90406d6e1e9c5f35',1,'otbr::rest::Json']]],
   ['dumpdbusmessage_892',['DumpDBusMessage',['../df/de9/namespaceotbr_1_1DBus.html#ae1ff1319b519ce23b219248e1b1e57f0',1,'otbr::DBus']]]
 ];
